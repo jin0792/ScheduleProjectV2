b8206dba7a3b759e5fa291b13d7109e2ba5cf61a
jin0792 <164024730+jin0792@users.noreply.github.com>
2025-04-02 19:40:34 +0900

    docs: README파일 작성

---
+++ src/main/java/com/example/scheduleprojectv2/ScheduleProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.example.scheduleprojectv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProjectV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scheduleprojectv2/ScheduleProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduleprojectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

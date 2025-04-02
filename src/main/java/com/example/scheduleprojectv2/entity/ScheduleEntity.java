362c4b32d45c9b4c198a9c14cb608eadbf56138c
jin0792 <164024730+jin0792@users.noreply.github.com>
2025-04-03 03:45:50 +0900

    feat: 주석 설명 추가

--- src/main/java/com/example/scheduleprojectv2/entity/ScheduleEntity.java
+++ src/main/java/com/example/scheduleprojectv2/entity/ScheduleEntity.java
@@ -19,6 +19,8 @@ public class ScheduleEntity extends BaseEntity {
     @Column(length = 100, nullable = false)
     private String contents;
 
+    // 연관관계 설정하기
+    // 한명의 유저는 여러개의 일정을 작성할 수 있다
     @ManyToOne
     @JoinColumn(name = "users_id")
     private UserEntity users;
